5e59b8cc9a8e33e7556c5b10385050ffed982ea6
bmf-san <bmf.infomation@gmail.com>
2024-07-02 21:57:29 +0900

    [update] add redis

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   set_current_tenant_through_filter # Required to opt into this behavior
 
   def index
+    Rails.cache.write('foo', 'bar')
+    @cached_value = Rails.cache.read('foo')
+    Rails.logger.info("Cached value: #{@cached_value}")
   end
 end
